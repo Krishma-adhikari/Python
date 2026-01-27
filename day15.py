2d3db92957e1e8d6421fbb7b94f87e6e22c9a98a
Krishma Adhikari <adhikarikrishma2@gmail.com>
2026-01-27 06:45:53 +0545

    new file

---
+++ day15.py
@@ -0,0 +1,19 @@
+# Create a python program capable of greeting you with Good Morning, Good Afternoon and Good Evening.
+#  Your program should use time module to get the current hour. 
+# Here is a sample program and documentation link for you:
+
+import time
+h = time.strftime("%H:%M:%S")
+h = int(time.strftime("%H"))
+
+if(4<=h>=12):
+    print("Good Morning Krishma",". The time is",h)
+
+elif(12<h>=17):
+    print("Good Afternoon Krishma",".The time is",h)
+elif(17<h>=20):
+    print("Good Evening Krishma",".The time is",h)
+else:
+    print("Good Night Krishma",". The time is",h)
+    
+ 

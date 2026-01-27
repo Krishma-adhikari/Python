0955e36f9ef1fc010b1fbf17a83ac685343b2ccb
Krishma Adhikari <adhikarikrishma2@gmail.com>
2026-01-27 06:52:11 +0545

    Added New Exercise

--- day15.py
+++ day15.py
@@ -4,9 +4,10 @@
 
 import time
 h = time.strftime("%H:%M:%S")
+print(h)
 h = int(time.strftime("%H"))
 
-if(4<=h>=12):
+if(4<=h<12):
     print("Good Morning Krishma",". The time is",h)
 
 elif(12<h>=17):
@@ -15,5 +16,5 @@ elif(17<h>=20):
     print("Good Evening Krishma",".The time is",h)
 else:
     print("Good Night Krishma",". The time is",h)
-    
+
  
